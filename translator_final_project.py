1feabb8262b9c509d89f8c4147e2a92c483d6539
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-02-06 22:03:54 +0800

    Create the function that will translate the inputted text

--- translator_final_project.py
+++ translator_final_project.py
@@ -3,7 +3,18 @@
 # import the necessary packages
 from googletrans import Translator, LANGUAGES
 
+
 # define the function that will translate the inputted text
+def translate_text(text, dest_language):
+    # create a translator object
+    translator = Translator()
+
+    # translate the inputted text to the desired language
+    translated_text = translator.translate(text, dest=dest_language)
+
+    # return the translated text
+    return translated_text.text
+
 
 # ask the user to input the text they would like to translate
 
