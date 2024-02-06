457da58eb4e270cc57aedc235fb0debd6559a1c1
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-02-06 21:57:11 +0800

    Add the pseudocode for the back-end

---
+++ translator_final_project.py
@@ -0,0 +1,15 @@
+# pseudocode
+
+# import the necessary packages
+
+# define the function that will translate the inputted text
+
+# ask the user to input the text they would like to translate
+
+# print the supported languages
+
+# ask the user to input the language they would like to translate to
+
+# call the function to translate the inputted text
+
+# print the translated text
