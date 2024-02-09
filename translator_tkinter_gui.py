88fa38ceb9c91c2a37b007323f6df63097f41b8a
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-02-09 16:55:43 +0800

    Refactor GUI layout and add padding to widgets

--- translator_tkinter_gui.py
+++ translator_tkinter_gui.py
@@ -43,25 +43,25 @@ original_text = tk.Label(
     text="Enter the text to be translated: ",
     font=("Montserrat Medium", 12),
 )
-original_text.pack()
+original_text.pack(padx=5, pady=5)
 
 # add the original text entry
 original_text_entry = tk.Entry(
     original_text_frame, width=25, font=("Century Gothic Bold", 12)
 )
-original_text_entry.pack()
+original_text_entry.pack(padx=5, pady=5)
 
 # add the desired language label
 dest_language = tk.Label(
     original_text_frame, text="Choose the language: ", font=("Montserrat Medium", 12)
 )
-dest_language.pack()
+dest_language.pack(padx=5, pady=5)
 
 # add combobox for the desired language
 dest_language_combobox = ttk.Combobox(
     original_text_frame, values=list(LANGUAGES.values())
 )
-dest_language_combobox.pack()
+dest_language_combobox.pack(padx=5, pady=5)
 
 # add the translate button
 translate_button = tk.Button(
@@ -70,7 +70,7 @@ translate_button = tk.Button(
     command=translate_text,
     font=("Arial Italic", 12),
 )
-translate_button.pack()
+translate_button.pack(padx=5, pady=5)
 
 # add frame for translated text
 translated_text_frame = tk.LabelFrame(
@@ -82,19 +82,19 @@ translated_text_frame.place(x=288, y=73)
 translated_text_label = tk.Label(
     translated_text_frame, text="Translated Text: ", font=("Montserrat Medium", 12)
 )
-translated_text_label.pack()
+translated_text_label.pack(padx=5, pady=5)
 
 # display the translated text
 translated_text = tk.Label(
     translated_text_frame, text="", font=("Century Gothic Bold", 12)
 )
-translated_text.pack()
+translated_text.pack(padx=5, pady=5)
 
 # add a done button
 done_button = tk.Button(
     translated_text_frame, text="Done", font=("Arial Italic", 10), command=window.quit
 )
-done_button.pack()
+done_button.pack(padx=5, pady=5)
 
 # add the main loop
 window.mainloop()
