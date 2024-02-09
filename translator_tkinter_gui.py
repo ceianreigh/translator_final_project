6e8791f7b3c03c5ced78e395fa739d647b015661
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-02-09 12:10:20 +0800

    Add original text entry widget

--- translator_tkinter_gui.py
+++ translator_tkinter_gui.py
@@ -16,5 +16,9 @@ original_text_frame.pack()
 original_text = tk.Label(original_text_frame, text="Enter the text to be translated: ")
 original_text.pack()
 
+# add the original text entry
+original_text_entry = tk.Entry(original_text_frame)
+original_text_entry.pack()
+
 # add the main loop
 window.mainloop()
